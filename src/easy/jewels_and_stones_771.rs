fc309803447df447cb802b1eff7fa9e0dcb1f6a2
flame4 <646361781@qq.com>
2019-01-08 00:07:00 +0800

    jewels_and_stones 2 lines solution!

--- src/easy/jewels_and_stones_771.rs
+++ src/easy/jewels_and_stones_771.rs
@@ -8,7 +8,6 @@ impl Solution {
         let bytes = j.into_bytes();
         let mut jewels_set = HashSet::new();
 
-        // TODO 运用迭代器来优化写法
         for i in 0..bytes.len() {
             jewels_set.insert(bytes[i]);
         }
@@ -20,5 +19,12 @@ impl Solution {
             }
         }
         num
+    }
+
+    // 网上看到的两行rust
+    pub fn num_jewels_in_stones_2line(j: String, s:String) -> i32 {
+        // TODO 为什么一定要显式声明?
+        let jewels: HashSet<char> = j.chars().collect();
+        s.chars().filter(|ch| jewels.contains(ch)).count() as i32
     }
 }
