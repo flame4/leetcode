5416e55e67cae379d84275af406bdf23e39cb45b
flame4 <646361781@qq.com>
2019-02-25 19:36:47 +0800

    sum of even number after queries and test, iterator version(fast and cool).

--- src/easy/sum_of_even_number_after_queries_985.rs
+++ src/easy/sum_of_even_number_after_queries_985.rs
@@ -1,9 +1,8 @@
 use super::Solution;
 
 impl Solution {
-
-    // v1: 练习迭代器版本, 每次改动重新求和, 肯定很慢...
-    pub fn sum_even_after_queries(a: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<i32> {
+    // v1: 练习迭代器版本, 每次改动重新求和, 肯定很慢... 104ms
+    pub fn sum_even_after_queries_v1(a: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<i32> {
         let mut a = a.clone();
         let mut ret = Vec::new();
         queries.iter().for_each(|x| {
@@ -14,8 +13,27 @@ impl Solution {
         });
         ret
     }
-}
 
+    // 同样思路, 改进一下, 20ms.
+    pub fn sum_even_after_queries(a: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut a = a.clone();
+        let mut ret = Vec::new();
+        let mut even_sum = a.iter().fold(0, |sum, val| {
+            if val % 2 == 0 { sum + val } else { sum }
+        });
+
+        queries.iter().for_each(|x| {
+            let (delta, index) = (x[0], x[1] as usize);
+            let old = a[index];
+            let new = old + delta;
+            a[index] = new;
+            if old % 2 == 0 { even_sum -= old }
+            if new % 2 == 0 { even_sum += new }
+            ret.push(even_sum);
+        });
+        ret
+    }
+}
 
 #[cfg(test)]
 mod tests {
