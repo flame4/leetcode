431b89e9d51d5cc68b31d84948bf1cc8717ede45
flame4 <646361781@qq.com>
2019-05-10 18:53:02 +0800

    Add ListNode Merge and test.

--- src/listnode.rs
+++ src/listnode.rs
@@ -43,6 +43,54 @@ impl ListNode {
         }
         ret
     }
+
+    /// 生序排序函数, o(nlogn)时间复杂度, 归并排序方法.
+    /// 因为快速排序涉及到单链条上的多可变引用修改, 在Rust无法用Safe做到.
+    /// 归并排序在拆分节点上可以做到拆开链条变成两条, 所以可以做到.
+    #[allow(dead_code)]
+    pub fn sort(&self) {}
+
+
+    /// 升序合并两个排好序的listnode, 归并排序使用.
+    pub(self) fn merge_list(mut l1: Option<Box<ListNode>>, mut l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut ret = ListNode::new(0);
+        let mut ret_pointer = &mut ret.next;
+        loop {
+            let mut l1_next = None;
+            let mut l2_next = None;
+            if let Some(n) = l1.as_mut() {
+                l1_next = n.next.take();
+            }
+            if let Some(n) = l2.as_mut() {
+                l2_next = n.next.take();
+            }
+
+            match (l1, l2) {
+                (Some(mut n1), Some(mut n2)) => {
+                    if n1.val < n2.val {
+                        ret_pointer.replace(n1);
+                        n2.next = l2_next;
+                        l2_next = Some(n2);
+                    } else {
+                        ret_pointer.replace(n2);
+                        n1.next = l1_next;
+                        l1_next = Some(n1);
+                    }
+                }
+                (Some(n), None) => {
+                    ret_pointer.replace(n);
+                }
+                (None, Some(n)) => {
+                    ret_pointer.replace(n);
+                }
+                (None, None) => break
+            }
+            l1 = l1_next;
+            l2 = l2_next;
+            ret_pointer = &mut ret_pointer.as_mut().unwrap().next;
+        }
+        ret.next
+    }
 }
 
 #[cfg(test)]
@@ -50,9 +98,16 @@ mod tests {
     use crate::listnode::ListNode;
 
     #[test]
-    pub fn listnode_vec_trans() {
+    pub fn listnode_vec_trans_test() {
         let v = vec![3, 2, 42, 2, 5, 2, 3, 4, 5, 2, 3, 4, 5, 2, 3, 4, 1, 1, 3, 1, 3];
         let root = ListNode::from_vec(v.clone());
         assert_eq!(root.unwrap().show_as_vec(), v);
+    }
+
+    #[test]
+    pub fn merge_list_test() {
+        let l1 = ListNode::from_vec(vec![1, 3, 5]);
+        let l2 = ListNode::from_vec(vec![2, 4, 6]);
+        assert_eq!(ListNode::merge_list(l1, l2).unwrap().show_as_vec(), vec![1, 2, 3, 4, 5, 6]);
     }
 }
--- src/medium/search_a_2d_matrix_ii_240.rs
+++ src/medium/search_a_2d_matrix_ii_240.rs
@@ -7,7 +7,7 @@ impl Solution {
     /// 思路1. 从左下角往右上角逼近，直到无法找到一个值. O(m+n)
     /// 思路2, 二分的思路, 先从对角线找起, 但是因为不是矩阵, 复杂度变为
     /// O(m/n*logn + n/(m - n *(m/n)*logm) 不是太漂亮...
-    pub fn search_matrix(nums: Vec<Vec<i32>>, target: i32) -> bool {
+    pub fn search_matrix(_nums: Vec<Vec<i32>>, _target: i32) -> bool {
         true
     }
 }
