d11a843c9c0c06437daa588028564577139b6c44
flame4 <646361781@qq.com>
2019-09-18 00:22:31 +0800

    add test for design hash map.

--- src/easy/design_hashmap_706.rs
+++ src/easy/design_hashmap_706.rs
@@ -12,22 +12,56 @@ struct MyHashMap {
 impl MyHashMap {
 
     /** Initialize your data structure here. */
-    fn new() -> Self {
+    #[allow(dead_code)]
+    pub(self) fn new() -> Self {
         MyHashMap {}
     }
 
     /** value will always be non-negative. */
-    fn put(&self, key: i32, value: i32) {
+    #[allow(dead_code)]
+    pub(self) fn put(&mut self, key: i32, value: i32) {
 
     }
 
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-    fn get(&self, key: i32) -> i32 {
-
+    #[allow(dead_code)]
+    pub(self) fn get(&mut self, key: i32) -> i32 {
+        0
     }
 
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-    fn remove(&self, key: i32) {
+    #[allow(dead_code)]
+    pub(self) fn remove(&mut self, key: i32) {
 
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::MyHashMap;
+
+    #[test]
+    pub fn my_hash_map_test() {
+        let mut map = MyHashMap::new();
+        map.put(1, 100);
+        map.put(100, 1);
+        for i in 0..30 {
+            map.put(1 << i, 0);
+        }
+
+        assert_eq!(map.get(0), -1);
+        assert_eq!(map.get(-1), -1);
+        assert_eq!(map.get(1), 100);
+        for i in 0..30 {
+            assert_eq!(map.get(1 << i), 0);
+        }
+
+        map.remove(100);
+        map.remove(1);
+        assert_eq!(map.get(100), -1);
+        assert_eq!(map.get(1), -1);
+    }
+}
+
+
